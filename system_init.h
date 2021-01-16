fdc28616a1a107fea9131a19ba34cbab7a447a31
ChipON-KungFu <ae@chipon-ic.com>
2021-01-16 22:09:35 +0800

    ●13 修改了整个GPIO_Output_LED ●12 system_init.c种SystemInit函数添加FLASH_CFG=0xC3配置,修改FLASH的读取周期为4周期 ●11 修改了整个GPIO_Output_LED ●10 更新IIC程序,更新IIC_DMA程序 ●9 更新FLASH程序,增加byte，word，doubleword的读写操作。删除老的单子（64bit）读写问题代码 ●8 为方便更新，标准库文件添加放到客户端处理 ●7 外部中断源的宏定义增加 ●6 INT_External_Source_Enable (uint32_t EintNum, uint32_tPeripheralSource)函数上的注释增加 ●5 修改TOUCH的位错误CT_CMPCFG_CMPPHASE_POS等16-27改为15-26 ●4 115200 z:8  x:1  y:15 ●3 GPIO_Pin_RMP_Config(GPIOF_SFR,GPIO_Pin_Num_1,GPIO_RMP_AF7_SPI1);//重映射为SPI3 ●2 增加#ifdef USE_CHECK_ASSERT ●1 增加#ifdef Perph_lcd_close

---
+++ system_init.h
@@ -0,0 +1,66 @@
+/**
+  ********************************************************************
+  * 文件名  system_init.h
+  * 作  者   ChipON_AE/FAE_Group
+  * 版  本  V2.2
+  * 日  期  2018-3-15
+  * 描  述  该文件提供了外设库包含的头文件与系统时钟配置相关的宏定义。
+  *
+  *********************************************************************
+*/
+#ifndef _SYSTEM_INIT_H_
+#define _SYSTEM_INIT_H_
+
+#include <string.h>
+#include <stdint.h>
+
+#include "KF32L_BASIC.h"
+#include "kf32l_basic_adc.h"
+#include "kf32l_basic_aes.h"
+#include "kf32l_basic_bkp.h"
+#include "kf32l_basic_can.h"
+#include "kf32l_basic_cfgl.h"
+#include "kf32l_basic_cmp.h"
+#include "kf32l_basic_crc.h"
+#include "kf32l_basic_dac.h"
+#include "kf32l_basic_dma.h"
+#include "kf32l_basic_exic.h"
+#include "kf32l_basic_flash.h"
+#include "kf32l_basic_gpio.h"
+#include "kf32l_basic_i2c.h"
+#include "kf32l_basic_int.h"
+#include "kf32l_basic_iwdt.h"
+#include "kf32l_basic_led.h"
+#include "kf32l_basic_lcd.h"
+#include "kf32l_basic_op.h"
+#include "kf32l_basic_osc.h"
+#include "kf32l_basic_pclk.h"
+#include "kf32l_basic_pm.h"
+#include "kf32l_basic_qei.h"
+#include "kf32l_basic_rst.h"
+#include "kf32l_basic_rtc.h"
+#include "kf32l_basic_spi.h"
+#include "kf32l_basic_sysctl.h"
+#include "kf32l_basic_systick.h"
+#include "kf32l_basic_tim.h"
+#include "kf32l_basic_usart.h"
+#include "kf32l_basic_usb.h"
+#include "kf32l_basic_wwdt.h"
+
+
+/* 系统时钟选择 */
+#ifndef SYSCLK_FREQ_HSE
+	#define SYSCLK_FREQ_HSI         //内部高频
+#endif
+
+/* 系统时钟选择 */
+#ifdef KF32L530
+//#define SYSCLK_FREQ_48MHz	48000000
+//#define SYSCLK_FREQ_72MHz	72000000
+//#define SYSCLK_FREQ_96MHz	96000000
+#define SYSCLK_FREQ_120MHz	120000000
+#endif
+
+static void SetSysClock(void);
+void SystemInit(void);
+#endif /* SYS_H_ */
