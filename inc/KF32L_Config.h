6333410a6db9cb1f237a44abac9dfa1ff25fcd7c
ChipON-KungFu <ae@chipon-ic.com>
2021-01-17 19:19:24 +0800

    ●39 增加systick延时函数 ●38 增加了料号 ●37 DAC改了

--- inc/KF32L_Config.h
+++ inc/KF32L_Config.h
@@ -2,7 +2,7 @@
   *********************************************************************
   * 文件名 KF32L_Config.h
   * 作  者  ChipON_AE/FAE_Group
-  * 版  本  V2.6
+  * 版  本  V2.61
   * 日  期  2019-11-16
   * 描  述  KF32L系列外设配置
   *
@@ -14,7 +14,11 @@
 
 #include "__Kungfu32_chipmodel_define.h"
 /* 型号定义 */
-#ifdef KF32L530KQT
+#ifdef KF32L530KNS
+	#define KF32L530
+#elif defined KF32L530MNS
+	#define KF32L530
+#elif defined KF32L530KQT
 	#define KF32L530
 #elif defined KF32L530MQT
     #define KF32L530
@@ -23,6 +27,18 @@
 #elif defined KF32L530KQV
 	#define KF32L530
 #endif
+#if defined KF32L530KQT || defined KF32L530MQT
+    #define L530_GPTIME_EX
+    #define L530_USART_EX
+    #define L530_IIC2_EX
+#elif defined KF32L530KQV || defined KF32L530MQV
+    #define L530_GPTIME_EX
+    #define L530_USART_EX
+    #define L530_IIC2_EX
+    #define L530_IIC3_EX
+	#define L530_SPI_EX
+    #define L530_EXTIC_EX
+#endif
 
 /* 外设资源 */
 #ifdef KF32L530
@@ -30,7 +46,6 @@
 	#define KF32L_Periph_adc
 	#define KF32L_Periph_adc0
 	#define KF32L_Periph_adc1
-	#define KF32L_Periph_adc2
  	/* AES */
 	#define KF32L_Periph_aes
 	/* CAN */
@@ -54,14 +69,10 @@
 	#define KF32L_Periph_dma
 	#define KF32L_Periph_dma0
 	#define KF32L_Periph_dma1
-	/* EXIC */
-	#define KF32L_Periph_exic
 	/* I2C */
     #define KF32L_Periph_i2c
     #define KF32L_Periph_i2c0
 	#define KF32L_Periph_i2c1
-	#define KF32L_Periph_i2c2
-	#define KF32L_Periph_i2c3
 	/* LCD */
 	#define KF32L_Periph_lcd
 	/* OP */
@@ -74,17 +85,12 @@
 	#define KF32L_Periph_spi0
 	#define KF32L_Periph_spi1
 	#define KF32L_Periph_spi2
-	#define KF32L_Periph_spi3
     /* USART */
 	#define KF32L_Periph_usart
 	#define KF32L_Periph_usart0
 	#define KF32L_Periph_usart1
 	#define KF32L_Periph_usart2
 	#define KF32L_Periph_usart3
-	#define KF32L_Periph_usart4
-	#define KF32L_Periph_usart5
-	#define KF32L_Periph_usart6
-	#define KF32L_Periph_usart7
     /* USB */
 	#define KF32L_Periph_usb
 	/* BTIME */
@@ -96,16 +102,38 @@
 	#define KF32L_Periph_gptime2
 	#define KF32L_Periph_gptime3
 	#define KF32L_Periph_gptime4
-	#define KF32L_Periph_gptime18
-	#define KF32L_Periph_gptime19
 	#define KF32L_Periph_gptime20
 	#define KF32L_Periph_gptime21
-	#define KF32L_Periph_gptime22_time23
-	#define KF32L_Periph_gptime22
-	#define KF32L_Periph_gptime23
 	/* ATIME */
 	#define KF32L_Periph_atime5_time6
 	#define KF32L_Periph_atime5
 	#define KF32L_Periph_atime6
 #endif //KF32L530
+#ifdef L530_EXTIC_EX
+	/* EXIC */
+	#define KF32L_Periph_exic
+#endif //L530_EXTIC_EX
+#ifdef L530_IIC2_EX
+	#define KF32L_Periph_i2c2
+#endif //L530_IIC2_EX
+#ifdef L530_IIC3_EX
+	#define KF32L_Periph_i2c3
+#endif //L530_IIC3_EX
+#ifdef L530_SPI_EX
+	#define KF32L_Periph_spi3
+#endif //L530_SPI_EX
+#ifdef L530_USART_EX
+	#define KF32L_Periph_usart4
+	#define KF32L_Periph_usart5
+	#define KF32L_Periph_usart6
+	#define KF32L_Periph_usart7
+#endif //L530_USART_EX
+#ifdef L530_GPTIME_EX
+	#define KF32L_Periph_gptime18
+	#define KF32L_Periph_gptime19
+	#define KF32L_Periph_gptime22_time23
+	#define KF32L_Periph_gptime22
+	#define KF32L_Periph_gptime23
+#endif //L530_GPTIME_EX
+
 #endif /* _KF32L_Config_H */
--- system_init.h
+++ system_init.h
@@ -2,7 +2,7 @@
   ********************************************************************
   * 文件名  system_init.h
   * 作  者   ChipON_AE/FAE_Group
-  * 版  本  V2.6
+  * 版  本  V2.61
   * 日  期  2018-3-15
   * 描  述  该文件提供了外设库包含的头文件与系统时钟配置相关的宏定义。
   *
@@ -63,4 +63,7 @@
 
 static void SetSysClock(void);
 void SystemInit(void);
+void systick_delay_init();
+void systick_delay_us(unsigned int nus);
+void systick_delay_ms(unsigned int mus);
 #endif /* SYS_H_ */
